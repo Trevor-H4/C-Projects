508ee27e3b784c289b28af7ac6e4e7142482171b
Trevor-H4 <60674670+Trevor-H4@users.noreply.github.com>
2020-02-04 16:29:07 -0600

    Update and rename PreLab2.c to Bigger and Smaller Function.c

--- Bigger and Smaller Function.c
+++ Bigger and Smaller Function.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void BigAndSmall(int number, int arr[], int *biggerNumbers, int *smallerNumbers);
+void BigAndSmall(int number, int arr[], int *biggerNumbers, int *smallerNumbers); // function prototype. this function
+// takes in an integer and returns which numbers are bigger and what numbers are smaller than it in the given array
 
 int main(void) {
 
@@ -9,7 +10,7 @@ int main(void) {
     int biggerNumbers, smallerNumbers, size = 65;
     
     
-    BigAndSmall(size, array, &biggerNumbers, &smallerNumbers);
+    BigAndSmall(size, array, &biggerNumbers, &smallerNumbers); // calling the function
 
     printf("\nAmount of numbers bigger than %d in the array: %d\n", size, biggerNumbers);
     printf("\nAmount of numbers smaller than %d in the array: %d\n", size, smallerNumbers);
@@ -25,15 +26,15 @@ void BigAndSmall(int number, int arr[], int *biggerNumbers, int *smallerNumbers)
     
       for (int i = 0; i < 10; i++) {
       if (arr[i] > number) {
-          bigger++;
+          bigger++; // adds one to bigger if conditions are met
         }
       
           if (arr[i] < number) {
-              smaller++;
+              smaller++; // adds one to smaller if conditions are met
           }
       }
-    *biggerNumbers = bigger;
-    *smallerNumbers = smaller;
+    *biggerNumbers = bigger; // sets int bigger equal to the pointer biggerNumbers
+    *smallerNumbers = smaller; // sets int smaller equal to the pointer smallerNumbers
 
     return;
 }
